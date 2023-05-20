b8ea1e9399a490fe0303350980fbecfd6bb81d9e
rabeehkk <rabeehkkofficial@gmail.com>
2023-05-20 12:12:40 +0530

    A simple login page  using Django

---
+++ registration/app1/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='login')
+
+def Homepage(request):
+    return render(request, 'home.html')
+
+def SignupPage(request):
+    if 'username' in request.session:
+        return redirect(Homepage)
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+
+        if pass1!=pass2:
+            return HttpResponse("Password does'nt match")
+        else:
+            my_user= User.objects.create_user(uname,email,pass1)
+            my_user.save()
+            return redirect('login')
+    return render(request,'register.html')
+
+
+def LoginPage(request):
+    if 'username' in request.session:
+        return redirect(Homepage)
+    if request.user.is_authenticated:
+        return redirect(Homepage)
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pass1=request.POST.get('password1')
+        user=authenticate(request,username=username,password=pass1)
+        if user is not None:
+            request.session['username']=username
+            login(request,user)
+            return render(request,'home.html')
+        else:
+            return HttpResponse("Please check your credentials,Wrong Username/Password")
+
+    return render(request,'login.html')
+
+def LogoutPage(request):
+    logout(request)
+    request.session.flush()
+    return redirect('login')
+
